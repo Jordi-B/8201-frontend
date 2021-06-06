81dfd752d262e1a3cbf5a982b2c180a162e4fdb1
Shuki <shukishuk7@gmail.com>
2021-06-06 23:04:59 +0300

    feat(): word editing and removing capabilities

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,8 +1,19 @@
 <template>
-    <div class="list-item">
-        <div class="text-h5 title">{{ title }}</div>
+    <!-- <div class="list-item">
+        <div class="text-h7 title">{{ title }}</div>
         <slot></slot>
-    </div>
+    </div> -->
+
+        <v-list-item class="list-item">
+            <v-list-item-icon v-if="true">
+                <v-icon @click="editItem" class="pencil">mdi-pencil</v-icon>
+                <v-icon @click="deleteItem" class="delete-button">mdi-delete</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+            <v-list-item-title class="title">{{ title }}</v-list-item-title>
+          </v-list-item-content>
+          <slot></slot>
+        </v-list-item>
 </template>
 
 <script>
@@ -13,19 +24,38 @@ export default {
             type: String,
             default: ''
         }
+    },
+    methods: {
+        deleteItem() {
+            alert('deleting...');
+        },
+
+        editItem() {
+            alert('editing...');
+        }
     }
 }
 </script>
 
 <style scoped>
 .list-item {
-    background-color: rgb(5, 5, 161);
+    background-color: rgb(61, 61, 131);
     text-align: right;
     margin-top: 3px;
+    border-radius: 2px;
 }
 
 .title {
     color: white;
     margin-right: 1vh;
 }
+
+.pencil {
+    color:rgb(238, 238, 238);
+}
+
+.delete-button {
+    color: rgb(236, 79, 79);
+}
+
 </style>
--- src/components/WordsList.vue
+++ src/components/WordsList.vue
@@ -1,7 +1,7 @@
 <template>
     <v-card class="words-list-card"
-    width="10vw"
-    height="80vh"
+    width="12vw"
+    height="90vh"
     elevation="11"
     outlined
     >
@@ -9,13 +9,38 @@
       <div class="text-h2 title">{{ title }}</div>
       <div class="title-description">{{ description }}</div>
     </v-card-text>
-    <v-btn class="new-item-button" color="purple" icon outlined depressed fab x-small dark>
+    <v-dialog
+    v-model="dialog"
+    width="500"
+    >
+    <template v-slot:activator="{ on }">
+    <v-btn class="new-item-button" v-on="on" v-if="true" color="purple" icon outlined depressed fab x-small dark>
       <v-icon>mdi-plus</v-icon>
     </v-btn>
+    </template>
+    
+    <v-card class="dialog-card">
+        <v-card-title
+        class="headline red lighten-2 dialog-title"
+        primary-title
+        >
+        הינך עומד להוסיף מילה חדשה למילים המנוטרות
+        </v-card-title>
+
+        <v-card-text>
+            מילה זו תיווסף למאגר המילים המנוטרות, חישבו היטב לפני הוספת מילה חדשה למאגר
+        </v-card-text>
+        <v-text-field class="dialog-input" v-model="newWord"></v-text-field>
+        <v-btn color="blue darken-1" text @click="dialog = false; newWord = ''">סגור</v-btn>
+        <v-btn color="blue darken-1" text @click="addNewWord">שמור</v-btn>
+    </v-card>
+    </v-dialog>
     <ListItem>
         <v-text-field 
         class="text-input" 
+        placeholder="חפש מילה"
         dark
+        dense
         v-model="search"
         ></v-text-field>
     </ListItem>
@@ -49,22 +74,40 @@ export default {
     },
     data() {
         return {
-            search: ''
+            search: '',
+            dialog: false,
+            newWord: ''
         }
     },
     computed: {
         filteredWords() {
             return this.words.filter(word => word.includes(this.search));
         }
+    },
+
+    methods: {
+        addNewWord() {
+            alert(this.newWord);
+            this.newWord = '';
+            this.dialog = false;
+        }
     }
 }
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+
+
+
 .words-list-card {
     background-color: #111633 !important;
     text-align: center;
-    border: 1.6px solid rgb(126, 126, 126) !important;
+    border: 2px solid rgb(172, 172, 172) !important;
+}
+
+.dialog-card {
+    text-align: center;
 }
 
 .title {
@@ -79,8 +122,8 @@ export default {
 }
 
 .new-item-button {
-    margin-left: 6.8vw;
     display: inline;
+    margin-bottom: 10px;
 }
 
 .search-field {
@@ -89,10 +132,13 @@ export default {
     direction: rtl;
 }
 
+.dialog-input {
+    direction: rtl;
+}
+
 .text-input {
     direction: rtl;
     width: 80%;
     margin-left: 2vh;
 }
-
 </style>
