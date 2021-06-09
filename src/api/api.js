d11d9ab3c05e2dea08aef2cf0cd8033ba6c9ee71
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 14:10:27 +0300

    feat(): added base url to api

--- src/api/api.js
+++ src/api/api.js
@@ -1,52 +1,55 @@
 import axios from 'axios';
+const BASE_URL = 'http://localhost:8083';
+// const PROD_SERVER = 'http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz';
+
 export default {
     lists() {
         return {
             wordsList: async () => {
-                return await axios.get('http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/words');
+                return await axios.get(`${BASE_URL}/api/words`);
             },
             suspectsList: async () => {
-                return await axios.get('http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/suspects');
+                return await axios.get(`${BASE_URL}/api/suspects`);
             },
             recentPosts: async () => {
-                return await axios.get('http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/posts/recent');
+                return await axios.get(`${BASE_URL}/api/posts/recent`);
             },
             recentSuspects: async () => {
-                return await axios.get('http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/suspects/new');
+                return await axios.get(`${BASE_URL}/api/suspects/new`);
             },
             recentWanted: async () => {
-                return await axios.get('http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/suspects/wanted/new');
+                return await axios.get(`${BASE_URL}/api/suspects/wanted/new`);
             },
             recentReleased: async () => {
-                return await axios.get('http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/suspects/released/new');
+                return await axios.get(`${BASE_URL}/api/suspects/released/new`);
             },
             addNewWord: async (newWord) => {
-                return await axios.post('http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/words/add', newWord);
+                return await axios.post(`${BASE_URL}/api/words/add`, newWord);
             },
             deleteWord: async (wordToDelete) => {
-                return await axios.delete('http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/words', {data: wordToDelete});
+                return await axios.delete(`${BASE_URL}/api/words`, {data: wordToDelete});
             },
             editWord: async (wordsToReplace) => {
-                return await axios.post('http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/words/replace', wordsToReplace);
+                return await axios.post(`${BASE_URL}/api/words/replace`, wordsToReplace);
             },
             postsAmount: async(profileId) => {
-                return await axios.get(`http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/posts/counts28/${profileId}`);
+                return await axios.get(`${BASE_URL}/api/posts/counts28/${profileId}`);
             }
         }
     },
     profile() {
         return {
             getLicenseById : async (id) => {
-                return await axios.get(`http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/licenses/${id}`)
+                return await axios.get(`${BASE_URL}/api/licenses/${id}`)
             },
             getNumOfPostsById : async (id) => {
-                return await axios.get(`http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/posts/person/count/${id}`)
+                return await axios.get(`${BASE_URL}/api/posts/person/count/${id}`)
             },
             getProfileById : async (id) => {
-                return await axios.get(`http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/suspects/suspect/${id}`)
+                return await axios.get(`${BASE_URL}/api/suspects/suspect/${id}`)
             },
             getPermissionsById : async (id) => {
-                return await axios.get(`http://intelligence-api-git-2-intelapp1.apps.openforce.openforce.biz/api/permissions/${id}`)
+                return await axios.get(`${BASE_URL}/api/permissions/${id}`)
             }
         }
     }
